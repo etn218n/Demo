69c5263b0ed841864a457a806e9e30f4901d9a43
Duy <etn218n@gmail.com>
2019-03-20 22:36:47 +0200

    Add Pig

---
+++ Assets/Scripts/Pig.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > 2.5f)
+            StartCoroutine(Disappear());
+    }
+
+    private IEnumerator Disappear()
+    {
+        anim.SetBool("Disappear", true);
+
+        yield return new WaitForSeconds(0.3f);
+
+        Destroy(this.gameObject);
+    }
+}
