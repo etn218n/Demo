57abe9685e63d2ab2b35e216786fb614f6e70178
Duy <etn218n@gmail.com>
2019-03-20 20:54:35 +0200

    Add UI to Game scene

--- Assets/MenuBehaviour.cs
+++ Assets/MenuBehaviour.cs
@@ -4,5 +4,7 @@ using UnityEngine.SceneManagement;
 [CreateAssetMenu(fileName = "MenuBehaviour", menuName = "Menu/MenuBehaviour")]
 public class MenuBehaviour : ScriptableObject
 {
-    public void LoadGameScene() { SceneManager.LoadScene("Game"); }
+    public void LoadGameScene()  { SceneManager.LoadScene("Game"); }
+    public void QuitGame()       { Application.Quit(); }
+    public void BacktoMainmenu() { SceneManager.LoadScene("Mainmenu"); }
 }
