c4c6de6a5d998056ff016d6eb9f2b9a7397e700a
Duy <etn218n@gmail.com>
2019-03-21 10:54:39 +0200

    Redesign

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -3,49 +3,62 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    private enum BirdState { Null, Init, Aim, Launch, Disappear }
+    private enum BirdState { Null, Idle, OnSlingshot, Aim, Fly, Disappear }
 
     private BirdState birdState;
 
     private bool MachineOn;
 
-    private bool MouseDragged;
-    private bool MouseReleased;
+    public bool LoadintoSlingshot;
+    public bool MouseDragged;
+    public bool MouseReleased;
 
-    private Rigidbody2D rb2d;
-    private Collider2D collider2d;
+    public Rigidbody2D rb2d      { get; private set; }
+    public Collider2D collider2d { get; private set; }
+
     private Animator anim;
 
     [SerializeField]
     private Transform aimPoint;
 
+    [Range(0, 20)]
+    public float flySpeed = 10;
+
     private void Awake()
     {
-        rb2d          = GetComponent<Rigidbody2D>();
-        anim          = GetComponent<Animator>();
-        collider2d    = GetComponent<Collider2D>();
+        rb2d       = GetComponent<Rigidbody2D>();
+        anim       = GetComponent<Animator>();
+        collider2d = GetComponent<Collider2D>();
     }
 
     private void Start()
     {
-        birdState = BirdState.Init;
+        birdState = BirdState.Idle;
         MachineOn = true;
+
         StartCoroutine(BirdStateMachine());
     }
 
-    private void OnMouseDrag() { MouseDragged  = true; }
-    private void OnMouseUp()   { MouseReleased = true; }
-
-    private IEnumerator Init()
+    private IEnumerator Idle()
     {
-        MouseDragged  = false;
-        MouseReleased = false;
+        SetAnimation(BirdState.Idle);
+
+        while (true)
+        {
+            if (LoadintoSlingshot) { birdState = BirdState.OnSlingshot; break; }
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator OnSlingshot()
+    {
+        SetAnimation(BirdState.OnSlingshot);
+
+        // Avoid OnMouseDrag conflict with Slingshot's Collider Aim
+        collider2d.enabled = false;
 
         rb2d.gravityScale = 0f;
-
-        gameObject.transform.right = aimPoint.position - this.gameObject.transform.position;
-
-        SetAnimation(BirdState.Init);
 
         while (true)
         {
@@ -60,31 +73,27 @@ public class Bird : MonoBehaviour
         // Enter State
         SetAnimation(BirdState.Aim);
 
-        //Update State
+        // Update State
         while (true)
         {
-            gameObject.transform.right = aimPoint.position - gameObject.transform.position;
-
-            Vector3 dragPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            rb2d.MovePosition(dragPos);
-
-            if (MouseReleased) { birdState = BirdState.Launch; break; }
+            if (MouseReleased) { birdState = BirdState.Fly; break; }
 
             yield return null;
         }
 
         //Exit State
+        collider2d.enabled = true;
     }
 
-    private IEnumerator Launch()
+    private IEnumerator Fly()
     {
         rb2d.gravityScale = 1f;
 
-        rb2d.velocity = (aimPoint.position - gameObject.transform.position) * 10f;
+        rb2d.velocity = (aimPoint.position - gameObject.transform.position) * flySpeed;
 
         yield return new WaitForSeconds(0.2f);
 
-        SetAnimation(BirdState.Launch);
+        SetAnimation(BirdState.Fly);
 
         float maxWaitTime = 4f;
         float waitTime    = 0f;
@@ -103,7 +112,7 @@ public class Bird : MonoBehaviour
     private IEnumerator Disappear()
     {
         collider2d.enabled = false;
-        rb2d.gravityScale  = 0f;
+        rb2d.gravityScale = 0f;
 
         SetAnimation(BirdState.Disappear);
 
@@ -119,10 +128,11 @@ public class Bird : MonoBehaviour
         {
             switch (birdState)
             {
-                case BirdState.Init:      yield return StartCoroutine(Init());      break;
-                case BirdState.Aim:       yield return StartCoroutine(Aim());       break;
-                case BirdState.Launch:    yield return StartCoroutine(Launch());    break;
-                case BirdState.Disappear: yield return StartCoroutine(Disappear()); break;
+                case BirdState.Idle:        yield return StartCoroutine(Idle());        break;
+                case BirdState.OnSlingshot: yield return StartCoroutine(OnSlingshot()); break;
+                case BirdState.Aim:         yield return StartCoroutine(Aim());         break;
+                case BirdState.Fly:         yield return StartCoroutine(Fly());         break;
+                case BirdState.Disappear:   yield return StartCoroutine(Disappear());   break;
             }
         }
     }
---
+++ Assets/Slingshot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private GameObject birdObject;
+    private Bird bird;
+
+    [SerializeField]
+    private Transform aimPoint;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        bird = birdObject.GetComponent<Bird>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            birdObject.transform.position = aimPoint.position;
+            bird.LoadintoSlingshot = true;
+            bird.rb2d.gravityScale = 0f;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 dragPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        rb2d.MovePosition(dragPos);
+
+        bird.transform.position = gameObject.transform.position;
+
+        bird.MouseDragged = true;
+    }
+
+    private void OnMouseUp()
+    {
+        bird.MouseReleased = true;
+    }
+
+}
