8f910ee951ca060a5a19bad4985bf6c527c5f528
Duy <etn218n@gmail.com>
2019-03-26 04:54:33 +0200

    Save Progress

---
+++ Assets/CursorBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CursorBehaviour : MonoBehaviour
+{
+    private Animator anim;
+
+    // Adjust tip of finger cursor to match hardware cursor
+    [SerializeField] private Vector2 offset = new Vector2(0.2f, -0.3f);
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position    = mousePosition + offset;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Clicked");
+            //anim.ResetTrigger("Clicked");
+        }
+
+    }
+}
