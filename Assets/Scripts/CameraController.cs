d6c2a150266951aa6ed75d34e9d0e2a49a866294
Duy <etn218n@gmail.com>
2019-04-12 11:59:11 +0300

    Fix Camera ratio

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float widthRatio  = 16f;
+    [SerializeField] private float heightRatio = 9f;
+
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        FixAspectRatio();
+    }
+
+    private void FixAspectRatio()
+    {
+        float targetAspect = widthRatio / heightRatio;
+
+        // determine the game window's current aspect ratio
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+
+        // current viewport height should be scaled by this amount
+        float scaleHeight = windowAspect / targetAspect;
+
+        // if scaled height is less than current height, add letterbox
+        if (scaleHeight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width  = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleHeight;
+
+            Rect rect = camera.rect;
+
+            rect.width  = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+    }
+}
